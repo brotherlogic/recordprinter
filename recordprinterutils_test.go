75b129fa9dab597ef5f0e7e26235f76dcf884cac
Simon Tucker <brotherlogic@gmail.com>
2018-09-24 07:25:44 -0700

    Adds the looping process.

--- recordprinterutils_test.go
+++ recordprinterutils_test.go
@@ -1,7 +1,53 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestBasic(t *testing.T) {
-	run()
+	"golang.org/x/net/context"
+
+	pbrm "github.com/brotherlogic/recordmover/proto"
+)
+
+type testBridge struct {
+	failMove  bool
+	failPrint bool
+}
+
+func (t *testBridge) getMoves(ctx context.Context) ([]*pbrm.RecordMove, error) {
+	if t.failMove {
+		return nil, fmt.Errorf("Built to fail")
+	}
+	return []*pbrm.RecordMove{&pbrm.RecordMove{InstanceId: int32(1234)}}, nil
+}
+func (t *testBridge) print(ctx context.Context, text string) error {
+	if t.failPrint {
+		return fmt.Errorf("Built to fail")
+	}
+	return nil
+}
+
+func InitTestServer() *Server {
+	s := Init()
+	s.SkipLog = true
+	return s
+}
+
+func TestMove(t *testing.T) {
+	s := InitTestServer()
+	s.bridge = &testBridge{}
+	s.moveLoop(context.Background())
+}
+
+func TestMoveFail(t *testing.T) {
+	s := InitTestServer()
+	s.bridge = &testBridge{failMove: true}
+	s.moveLoop(context.Background())
+}
+
+func TestPrintFail(t *testing.T) {
+	s := InitTestServer()
+	s.bridge = &testBridge{failPrint: true}
+	s.moveLoop(context.Background())
+
 }
