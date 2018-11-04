3fd02150a70de0500e566ce70e2d611c34229796
Simon Tucker <simon@brotherlogic@gmail>
2018-11-03 18:00:57 -0700

    Making a change

--- recordprinter.go
+++ recordprinter.go
@@ -262,7 +262,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
 	return []*pbg.State{
-		&pbg.State{Key: "count", Value: s.count},
+		&pbg.State{Key: "curr_count", Value: s.count},
 		&pbg.State{Key: "last_count", Text: fmt.Sprintf("%v", s.lastCount)},
 	}
 }
