df5455dcca357c2e8500613b56f91673083f838e
Simon Tucker <simon@brotherlogic@gmail>
2018-12-07 10:04:11 -0800

    Updates logging. This closes #31

--- recordprinterutils.go
+++ recordprinterutils.go
@@ -27,6 +27,7 @@ func (s *Server) moveLoop(ctx context.Context) {
 		}
 
 		//Raise an alarm if the move context is incomplete
+		s.Log(fmt.Sprintf("Testing %v and %v", move.GetBeforeContext(), move.GetAfterContext()))
 		if (move.GetBeforeContext() == nil || move.GetAfterContext() == nil) || (move.GetBeforeContext().Before == nil && move.GetBeforeContext().After == nil) || (move.GetAfterContext().Before == nil && move.GetAfterContext().After == nil) {
 			s.Log(fmt.Sprintf("Move : %v", move))
 			s.RaiseIssue(ctx, "Context is missing from move", fmt.Sprintf("Move regarding %v is missing the full context %v -> %v", move.InstanceId, move.BeforeContext, move.AfterContext), false)
