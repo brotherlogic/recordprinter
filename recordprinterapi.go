1aa5d1f96ee742f649c97e833a128b95ff9742bd
Simon Tucker <brotherlogic@gmail.com>
2024-09-02 17:13:24 -0700

    Removes printer

--- recordprinterapi.go
+++ recordprinterapi.go
@@ -6,7 +6,7 @@ import (
 	rcpb "github.com/brotherlogic/recordcollection/proto"
 )
 
-//ClientUpdate on an updated record
+// ClientUpdate on an updated record
 func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest) (*rcpb.ClientUpdateResponse, error) {
-	return &rcpb.ClientUpdateResponse{}, s.moveLoop(ctx, req.GetInstanceId())
+	return &rcpb.ClientUpdateResponse{}, nil
 }
